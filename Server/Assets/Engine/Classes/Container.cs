84fcacf3f26393aaa549a2f95b85c5f3c30bac14
amandani <amandani@ramapo.edu>
2021-02-23 03:21:32 -0500

    Server Side container system with item defintion started

---
+++ Server/Assets/Engine/Classes/Container.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+
+public class Container
+{
+    public List<Item> ContainerItems { get; set; }
+
+    public bool DeleteOnRefresh { get; set; }
+   
+
+    /// <summary>
+    /// Dynamic container changes. This is the only constructor that should delete on refresh
+    /// </summary>
+    public Container()
+    {
+        DeleteOnRefresh = true;
+        ContainerItems = new List<Item>();
+    }
+    /// <summary>
+    /// Container with empty items for all slots
+    /// </summary>
+    /// <param name="MaxCapacity"></param>
+    private Container(int MaxCapacity)
+    {
+        ContainerItems = new List<Item>();
+        for (int i = 0; i < MaxCapacity; i++)
+        {
+            ContainerItems.Add(new Item());
+        }
+        DeleteOnRefresh = false;
+    }
+
+    public void AddItem(Item item)
+    {
+        var ItemAtSlot = GetItem(item.ItemName);
+        if (ItemAtSlot != null)
+        {
+            if (ItemAtSlot.IsStackable)
+            {
+
+            }
+        }
+    }
+
+    public void RemoteItem(int slot)
+    {
+        if (slot + 1 > ContainerItems.Count)
+        {
+            UnityEngine.Debug.Log("cant remove out of capacity");
+            return;
+        }
+        if (DeleteOnRefresh)
+        {
+            ContainerItems.RemoveAt(slot);
+        } else
+        {
+            ContainerItems[slot].Clear();
+        }
+    }
+    public void RemoveItem(Item a_item)
+    {
+        if (ContainsItem(a_item))
+        {
+            var myItem = GetItem(a_item.ItemName);
+            ContainerItems.Remove(myItem);
+        }
+    }
+
+    public bool ContainsItem(Item a_item)
+    {
+        foreach(Item item in ContainerItems) {
+            if (item.ItemName.ToLower().Equals(a_item.ItemName.ToLower()))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Item GetItem(string a_name)
+    {
+        foreach (Item item in ContainerItems)
+        {
+            if (item.ItemName.ToLower().Equals(a_name.ToLower()))
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+}
---
+++ Server/Assets/Engine/Classes/Item.cs
@@ -0,0 +1,48 @@
+﻿public class Item {
+
+    public string ItemName { get; set; }
+    public int ItemLevel { get; set; }
+    public int Amount { get; set; }
+    public bool IsStackable { get; set; } = false;
+
+    //Empty Item
+    public Item()
+    {
+        ItemName = "";
+        ItemLevel = -1;
+        Amount = -1;
+    }
+
+    //Base item with defined name and default level and amount
+    public Item(string a_name)
+    {
+        ItemName = a_name;
+        ItemLevel = 1;
+        Amount = 1;
+    }
+    
+    //Constructor with items level and default amount
+    public Item(string a_name, int a_level)
+    {
+        ItemName = a_name;
+        ItemLevel = a_level;
+        Amount = 1;
+    }
+
+
+    //Constructor with items level and amount
+    public Item(string a_name, int a_level, int a_amount)
+    {
+        ItemName = a_name;
+        ItemLevel = a_level;
+        Amount = a_amount;
+    }
+
+    public void Clear()
+    {
+        ItemName = "";
+        ItemLevel = -1;
+        Amount = -1;
+    }
+    
+}
