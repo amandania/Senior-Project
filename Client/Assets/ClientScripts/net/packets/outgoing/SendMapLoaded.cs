3c7fd7d7114a94485355d4efdb11fd8dff021ab9
amandani <amandani@ramapo.edu>
2021-03-20 00:42:10 -0400

    Documentation on client part 8

--- Client/Assets/ClientScripts/net/packets/outgoing/SendMapLoaded.cs
+++ Client/Assets/ClientScripts/net/packets/outgoing/SendMapLoaded.cs
@@ -1,8 +1,14 @@
 ﻿using DotNetty.Buffers;
-
+/// <summary>
+/// This class is used as an empty packet signal to let server know we are done loading the map.
+/// </summary>
 public class SendMapLoaded : IOutgoingPacketSender
 {
 
+    /// <summary>
+    /// Empty buffer is created containg packet id only
+    /// </summary>
+    /// <returns>Buffer for server</returns>
     public IByteBuffer CreatePacket()
     {
         IByteBuffer buffer = Unpooled.Buffer();
@@ -11,6 +17,10 @@ public class SendMapLoaded : IOutgoingPacketSender
         return buffer;
     }
 
+    /// <summary>
+    /// Packet Id used to refrence the mapping on server to handle incoming packets.
+    /// <return>Enum ordinal for outgoing map loaded packet</return>
+    /// </summary>
     public OutgoingPackets PacketType => OutgoingPackets.SEND_MAP_LOADED;
 
 }
